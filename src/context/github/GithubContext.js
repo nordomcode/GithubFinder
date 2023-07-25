6efb87d37ebedf5dddfd3ae6e12a9407abbfc261
nordomecode <dumpyreg@gmail.com>
2023-07-25 17:27:49 +1000

    Add search

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,51 +1,50 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer } from 'react'
 import githubReducer from './GithubReducer'
-
 const GithubContext = createContext()
-
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
-
-export const GithubProvider = ({children}) => {
+export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
-    loading: false
+    loading: false,
   }
 
   const [state, dispatch] = useReducer(githubReducer, initialState)
 
-    //Get initial users(testing purposes)
-    const fetchUsers = async () => {
-      
-      setLoading()
+  // Get search results
+  const searchUsers = async (text) => {
+    setLoading()
 
-      const response = await fetch(`${GITHUB_URL}/users`, {
-        headers: {
-          Authorization: `token ${GITHUB_TOKEN}`,
-        },
-      })
-    
-      const data = await response.json()
-    
-      dispatch({
-        type: 'GET_USERS',
-        payload: data,        
-      })
-
-    }
-
-    //Set loading
-    const setLoading = () => dispatch({
-      type: 'SET_LOADING'
+    const params = new URLSearchParams({
+      q: text,
     })
 
-    return <GithubContext.Provider value = {{
-        users: state.users,
-        loading: state.loading ,
-        fetchUsers
-    }}>
-        {children}
-    </GithubContext.Provider>
-}
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+      },
+    })
 
-export default GithubContext;
+    const { items } = await response.json()
+
+    dispatch({
+      type: 'GET_USERS',
+      payload: items,
+    })
+  }
+
+  // Set loading
+  const setLoading = () => dispatch({ type: 'SET_LOADING' })
+  return (
+    <GithubContext.Provider
+      value={{
+        users: state.users,
+        loading: state.loading,
+        searchUsers,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  )
+}
+export default GithubContext
