11c4df9edbe600e3cfccf57db7efcce4f185b7e7
nordomecode <dumpyreg@gmail.com>
2023-07-28 15:10:22 +1000

    Add getUser function

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ import Alert from './components/layout/Alert'
 import Home from './pages/Home'
 import About from './pages/About'
 import NotFound from './pages/NotFound'
+import User from './pages/User'
 
 import {GithubProvider} from './context/github/GithubContext'
 import {AlertProvider} from './context/alert/AlertContext'
+
+
 
 
 function App() {
@@ -24,6 +27,7 @@ function App() {
             <Routes>
               <Route path='/' element={<Home />} />
               <Route path='/about' element={<About />} />
+              <Route path='/user/:login' element={<User />}/>
               <Route path='/notfound' element={<NotFound />} />
               <Route path='/*' element={<NotFound />} />
             </Routes>
