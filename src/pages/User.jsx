c470254196e9e7fef8164248c0891ac4a2c37258
nordomecode <dumpyreg@gmail.com>
2023-07-29 16:44:30 +1000

    Refactoring Context and adding Actions

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -7,18 +7,29 @@ import {useParams, Link} from 'react-router-dom'
 import Spinner from '../components/layout/Spinner'
 import RepoList from '../components/repos/RepoList'
 
+import {getUser, getRepos} from '../context/github/GithubActions'
+
 function User() {
 
-    const {getUser, user, loading, getRepos, repos} = useContext(GithubContext)
+    const {user, loading, repos, dispatch} = useContext(GithubContext)
 
     const params = useParams()
 
     useEffect(() => {
-        getUser(params.login)
 
-        getRepos(params.login)
+        dispatch({type: 'SET_LOADING'})
+
+        const getUserData =  async () => {
+            const userData = await getUser(params.login)
+            dispatch({type: 'GET_USER', payload: userData})
+
+            const userRepoData = await getRepos(params.login)
+            dispatch({type: 'GET_REPOS', payload: userRepoData})
+        }
+
+        getUserData()
         
-    }, [])
+    }, [dispatch, params.login])
 
     const {
         name,
